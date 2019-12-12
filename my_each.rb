f3caa24676bf150b3e1be70510d73adc5d5dc9d8
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 16:31:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,11 @@
 def my_each(array)
+  
   index = 0 
   
-  while index < array.length 
+ while index < array.length 
   yield(array[index])
   index += 1 
- end
- 
- end
- 
- array = ["Bob", "Sarah", "Tim"]
- 
- my_each(array) do |name|
-   puts name 
+
  end 
  return array 
  end
